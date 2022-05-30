f84a5d54335eea65662923d9e51dd4bb56e926c7
Brylan Gannon <brylan.gannon112@gmail.com>
2022-05-30 12:35:49 -0600

    Test: bachelorette show page has bachelorette info

---
+++ spec/features/bachelorette/show_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe 'The Bachelorette Show Page' do
+  it "shows the bachelorette's attributes" do
+    @bach1 = Bachelorette.create!(name: 'Timbo', season_number: 69, description: 'stupid ass show')
+    @bach2 = Bachelorette.create!(name: 'JIMBO', season_number: 4, description: 'dumb show')
+
+    visit "/bachelorettes/#{@bach1.id}"
+
+    expect(page).to have_content 'Timbo'
+    expect(page).to_not have_content 'JIMBO'
+
+    expect(page).to have_content 'Season Number: 69'
+    expect(page).to_not have_content 'Season Number: 4'
+
+    expect(page).to have_content 'Description: stupid ass show '
+    expect(page).to_not have_content 'Description: dumb show'
+  end
+end
