577e6a5fbf10647e4008ad78b18a14cadf2c09a7
Brylan Gannon <brylan.gannon112@gmail.com>
2022-05-30 13:04:47 -0600

    Feat: bachelorette show has links to contestant index

---
+++ app/controllers/bachelorette_contestants_controller.rb
@@ -0,0 +1,5 @@
+class BacheloretteContestantsController < ApplicationController
+  def index
+    
+  end
+end
--- spec/features/bachelorette/show_spec.rb
+++ spec/features/bachelorette/show_spec.rb
@@ -1,12 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe 'The Bachelorette Show Page' do
-  it "shows the bachelorette's attributes" do
+  before :each do
     @bach1 = Bachelorette.create!(name: 'Timbo', season_number: 69, description: 'stupid ass show')
     @bach2 = Bachelorette.create!(name: 'JIMBO', season_number: 4, description: 'dumb show')
 
     visit "/bachelorettes/#{@bach1.id}"
+  end
 
+  it "shows the bachelorette's attributes" do
     expect(page).to have_content 'Timbo'
     expect(page).to_not have_content 'JIMBO'
 
@@ -15,5 +17,11 @@ RSpec.describe 'The Bachelorette Show Page' do
 
     expect(page).to have_content 'Description: stupid ass show'
     expect(page).to_not have_content 'Description: dumb show'
+  end
+
+  it 'has a link to see the bachelorette contestants' do
+    click_link 'View Contestants'
+
+    expect(current_path).to eq("/bachelorettes/#{@bach1.id}/contestants")
   end
 end
